0f9a5e5ed412f27ad007823a0efafb6a8c142e16
d1azdn <diazsaputramc@gmail.com>
2024-04-12 17:33:08 +0700

    Fixed bugs on main.py

--- imgSharpen.py
+++ imgSharpen.py
@@ -5,31 +5,32 @@ class sharpening:
     def __init__(self):
         pass
     def morphEx(self, image):
-        img = cv2.resize(image, (450,250))
+        img = cv2.imread(image)
         imgArray = np.array(img)
         grey = cv2.cvtColor(imgArray,cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(grey,(5,5),0)
         dilated = cv2.dilate(blur,np.ones((3,3)))
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
         closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
         cv2.imwrite("imageEnhanced.png", closing)
         return closing
     
-    def unsharpMask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
-        blurred = cv2.GaussianBlur(image, kernel_size, sigma)
-        sharpened = float(amount + 1) * image - float(amount) * blurred
+    def unsharpMask(self, image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+        img = cv2.imread(image)
+        blurred = cv2.GaussianBlur(img, kernel_size, sigma)
+        sharpened = float(amount + 1) * img - float(amount) * blurred
         sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
         sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
         sharpened = sharpened.round().astype(np.uint8)
         if threshold > 0:
-            low_contrast_mask = np.absolute(image - blurred) < threshold
-            np.copyto(sharpened, image, where=low_contrast_mask)
+            low_contrast_mask = np.absolute(img - blurred) < threshold
+            np.copyto(sharpened, img, where=low_contrast_mask)
         cv2.imwrite("imageEnhanced.png", sharpened)
         return sharpened
     
-    def gaussThresh(image):
+    def gaussThresh(self, image):
         img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-        blur = cv2.GaussianBlur(img, 5)
+        blur = cv2.GaussianBlur(img,(5,5),0)
         thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
         cv2.imwrite("imageEnhanced.png", thresh)
         return thresh
--- main.py
+++ main.py
@@ -7,28 +7,30 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_option_menu import option_menu
 
-def imageOCR(self,image,method):
+def imageOCR(image,method):
     match method:
         case "morphEx" :
             img = imgSharpen.sharpening()
             img = img.morphEx(image)
+            st.session_state.enhanced = "./imageEnhanced.png"
         case "unsharpMask" :
             img = imgSharpen.sharpening()
             img = img.unsharpMask(image)
+            st.session_state.enhanced = "./imageEnhanced.png"
         case "gaussThresh" : 
             img = imgSharpen.sharpening()
             img = img.gaussThresh(image)
+            st.session_state.enhanced = "./imageEnhanced.png"
         case None : 
             img = image
     
     reader = easyocr.Reader(['en'], gpu=True)
     readText = reader.readtext(img)
     text = ""
-    for t in text:
+    for t in readText:
         text = text + t[1] + " "
     
     st.session_state.text = text
-    st.session_state.enhanced = "imageEnhanced.png"
 
 
 fileCheck = [f for f in os.listdir("./examples") if f.endswith(".png") 
@@ -69,6 +71,8 @@ class streamlitContainer :
         st.subheader(input['title'])
         st.text(input['view'])
         st.write(input['author'])
+##########
+
 
 header = streamlitContainer()
 header.container(2)
@@ -81,24 +85,61 @@ with header.right:
     if st.button("Start now!"):
         pass
 
+
 st.markdown("#")
 st.write("---")#####
 st.markdown("#")
+
 
 main = streamlitContainer()
 main.container(2)
 with main.left : 
     file = st.selectbox('Select your image. (png/jpg/jpeg)', fileCheck)
     st.image("./examples/"+file)
+    choice = None
     if st.checkbox("Include sharpening image"):
-        choice = st.radio("--- Choose method ---",["morphEx", "unsharpMask", "gaussThresh"],
-                          captions=['','','Just make it b&w'])
+        st.warning("If the generated text isn't right, please turn off Sharpening Image.")
+        choice = st.radio("--- Choose method ---",["unsharpMask", "morphEx", "gaussThresh"],
+                          captions=['(Recommended)','',''], index=None)
     if st.button("Generate"):
-        imageOCR("./examples/"+file)
+        imageOCR("./examples/"+file, choice)
 
 with main.right:
-    st.write("Your text will show here.")
-    st.code(st.session_state.text)
+    st.text_area("Your text will show here.", st.session_state.text)
     st.write("---")
     st.write("Sharpened image : ")
     st.image(st.session_state.enhanced)
+
+
+st.markdown("#")
+st.write("---")#####
+st.markdown("#")
+
+
+bottom = streamlitContainer()
+bottom.container(2)
+with bottom.left : 
+    st.title("What is ittX ?")
+    st.write("(ittX - imagetoText) is an application that convert image into text, with help of python package : EasyOCR.")
+    st.write("And also there are features to enhance your image too!")
+
+with bottom.right:
+    bottom.iconMid("https://icons.iconarchive.com/icons/dtafalonso/android-l/256/Youtube-icon.png")
+
+
+st.markdown("#")
+st.write("---")#####
+st.markdown("#")
+
+
+footer = streamlitContainer()
+footer.container(2)
+with footer.right : 
+    st.title("About this Project")
+    st.write("This is my **second** project for python language. I will happy to accept any suggestion you share with me ^^.")
+    st.write("My Github page linked down below.")
+    if st.button("See more on Github ->") :
+        webbrowser.open("https://github.com/d1azdn")
+
+with footer.left:
+    footer.iconMid("https://cdn-icons-png.flaticon.com/512/25/25231.png")
